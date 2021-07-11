405fd480fb0b25a27f4ce567125f7c294ebff399
SJellen <sjellen@me.com>
2021-07-11 15:05:02 -0400

    work on the ImageNav component

--- components/BestGear.js
+++ components/BestGear.js
@@ -10,13 +10,13 @@ export default function BestGear() {
                 <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
             </div>
             <div className={styles.imgContainer}>
-                <Image src="/assets/shared/desktop/image-best-gear.jpg" alt="man with headphones" width="540" height="588"  className={styles.svg}/>
+                <Image src="/assets/shared/desktop/image-best-gear.jpg" alt="man with headphones" width="540" height="588"  className={styles.img}/>
             </div>
             <div className={styles.imgContainerTablet}>
-                <Image src="/assets/shared/tablet/image-best-gear.jpg" alt="man with headphones" width="689" height="300"  className={styles.svg}/>
+                <Image src="/assets/shared/tablet/image-best-gear.jpg" alt="man with headphones" width="689" height="300"  className={styles.img}/>
             </div>
             <div className={styles.imgContainerMobile}>
-                <Image src="/assets/shared/mobile/image-best-gear.jpg" alt="man with headphones" width="327" height="300"  className={styles.svg}/>
+                <Image src="/assets/shared/mobile/image-best-gear.jpg" alt="man with headphones" width="327" height="300"  className={styles.img}/>
             </div>
 
 
--- components/ImageNav.js
+++ components/ImageNav.js
@@ -1,8 +1,47 @@
 import styles from '../styles/ImageNav.module.scss'
+import Image from 'next/image'
 
 export default function ImageNav() {
 
     return (
-        <div className={styles.container}>imageNav</div>
+        <div className={styles.container}>
+            <div className={styles.selectionBox}>
+                <div className={styles.grayBacking}></div>
+                <div className={styles.foregroundFlexContainer}>
+                <div className={styles.imgContainer}>
+                    <Image src="/assets/shared/desktop/image-headphones.png" alt="headphones" width="200" height="200"  className={styles.img}/>
+                </div>
+                <h3>HEADPHONES</h3>
+                <h4>SHOP <span>
+                    <Image src="/assets/shared/desktop/icon-arrow-right.svg" alt="right arrow" width="5" height="10"  className={styles.svg}/>
+                </span></h4>
+                </div>
+            </div>
+            <div className={styles.selectionBox}>
+                <div className={styles.grayBacking}></div>
+                <div className={styles.foregroundFlexContainer}>
+                <div className={styles.imgContainer}>
+                    <Image src="/assets/shared/desktop/image-speakers.png" alt="speakers" width="200" height="200"  className={styles.img}/>
+                </div>
+                <h3>SPEAKERS</h3>
+                <h4>SHOP <span>
+                    <Image src="/assets/shared/desktop/icon-arrow-right.svg" alt="right arrow" width="5" height="10"  className={styles.svg}/>
+                </span></h4>
+                </div>
+                
+            </div>
+            <div className={styles.selectionBox}>
+                <div className={styles.grayBacking}></div>
+                <div className={styles.foregroundFlexContainer}>
+                <div className={styles.imgContainer}>
+                    <Image src="/assets/shared/desktop/image-earphones.png" alt="earphones" width="200" height="200"  className={styles.img}/>
+                </div>
+                <h3>EARPHONES</h3>
+                <h4>SHOP <span>
+                    <Image src="/assets/shared/desktop/icon-arrow-right.svg" alt="right arrow" width="5" height="10"  className={styles.svg}/>
+                </span></h4>
+                </div>
+            </div>
+        </div>
     )
 }

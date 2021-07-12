b32cee75c8385949d5848dfd451d69e2a1ba11dd
SJellen <sjellen@me.com>
2021-07-12 15:44:07 -0400

    Home/Hero set

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -13,7 +13,11 @@ export default function Hero() {
             <div className={styles.imgContainerMobile}>
                 <Image src="/assets/home/mobile/image-header.jpg" alt="headphones" width="750" height="1200"  className={styles.img}/>
             </div>
-            <div className={styles.textContainer}>
+            <div className={styles.textContainer}>  
+                <span>NEW PRODUCT</span>
+                <h1>XX99 Mark II Headphones</h1>
+                <p>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                <a>SEE PRODUCT</a>
 
             </div>
         

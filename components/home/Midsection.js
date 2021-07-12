2550349672a99d767c00938957af793c0d491bfb
SJellen <sjellen@me.com>
2021-07-12 15:57:30 -0400

    set up on home/MidSection

---
+++ components/home/Midsection.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/home/Midsection.module.scss'
+
+export default function Midsection() {
+
+    return (
+        <div className={styles.container}>Midsection</div>
+    )
+
+}
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 import ImageNav from '../components/ImageNav'
 import Hero from '../components/home/Hero'
+import Midsection from '../components/home/Midsection'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Hero />
       <ImageNav />
+      <Midsection />
     </div>
   )
 }

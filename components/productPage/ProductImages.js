b11c4e49eaf2bffded4ac2af3880f1116ff78b80
SJellen <sjellen@me.com>
2021-07-16 13:37:41 -0400

    layout of image containers

--- components/productPage/ProductImages.js
+++ components/productPage/ProductImages.js
@@ -1,7 +1,15 @@
 import styles from '../../styles/productPage/ProductImages.module.scss'
+import Image from 'next/image'
+
 export default function ProductImages() {
 
     return (
-        <div className={styles.container}>product images</div>
+        <div className={styles.container}>
+            <div className={styles.leftContainer}>
+                <div className={styles.topLeftContainer}></div>
+                <div className={styles.bottomLeftContainer}></div>
+            </div>
+            <div className={styles.rightContainer}></div>
+        </div>
     )
 }

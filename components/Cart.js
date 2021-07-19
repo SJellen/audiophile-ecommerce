73cc2cb57dce80855d8fe9a1da9710c31f9677a1
SJellen <sjellen@me.com>
2021-07-19 14:55:26 -0400

    work on item layout in Cart

--- components/Cart.js
+++ components/Cart.js
@@ -1,19 +1,33 @@
 import React, {useContext} from 'react'
 import styles from '../styles/Cart.module.scss'
 import { Context} from '../context/Context'
+import useCartLogic from '../logic/useCartLogic'
+import Image from 'next/image'
+import image from 'next/image'
 
 
 export default function Cart() {
 
     const {isCartOpen, cart} = useContext(Context)
+    const {filteredCart, handleRemoveAllClick} = useCartLogic()
 
-    console.log(cart)
+
+    const productMapping = filteredCart.map((item, index) => (
+        <div key={item[index]} className={styles.itemContainer}>
+            <div className={styles.imgContainer}>
+                <Image src={item.image} alt="product thumbnail" width="150" height="150"  className={styles.img}/>
+            </div>
+           
+
+        </div>
+    ))
     
     return (
         <div className={styles.container} style={{display: isCartOpen ? "" : "none"}}>
             <div className={styles.topCartContainer}>
-                <h2>Cart (3)</h2> <h3>Remove all</h3>
+                <h2>Cart ({filteredCart.length})</h2> <h3 onClick={() => handleRemoveAllClick()}>Remove all</h3>
             </div>
+            {productMapping}
         </div>
     )
 }
--- context/Context.js
+++ context/Context.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import data from '../data/data.json'
+import items from '../data/cart.json'
 
 const Context = React.createContext() 
 
@@ -49,50 +50,9 @@ function ContextProvider({ children }) {
         lineBreakFix()
     }, [currentProduct])
 
-    const [cart, setCart] = useState([
-        {
-            name: "YX1 Wireless Earphones",
-            displayName: "YX1",
-            price: 599,
-            image: "/assets/cart/image-yx1-earphones.jpg",
-            quantity: 0
-        },
-        {
-            name: "XX59 Headphones",
-            displayName: "XX59",
-            price: 899,
-            image: "/assets/cart/image-xx59-headphones.jpg",
-            quantity: 0
-        },
-        {
-            name: "XX99 Mark I Headphones",
-            displayName: "XX99 MKI",
-            price: 1750,
-            image: "/assets/cart/image-x99-mark-one-headphones.jpg",
-            quantity: 0
-        },
-        {
-            name: "XX99 Mark II Headphones",
-            displayName: "XX99 MKII",
-            price: 2999,
-            image: "/assets/cart/image-xx99-mark-two-headphones.jpg",
-            quantity: 0
-        },
-        {
-            name: "ZX7 Speaker",
-            displayName: "ZX7",
-            price: 3500,
-            image: "/assets/cart/image-zx7-speaker.jpg",
-            quantity: 0
-        },
-        {
-            name: "ZX9 Speaker",
-            displayName: "ZX9",
-            price: 4500,
-            image: "/assets/cart/image-zx9-speaker.jpg",
-            quantity: 0
-        }
-    ])
+    const initialCart = items
+
+    const [cart, setCart] = useState(initialCart)
 
 
     function handleAddToCartClick() {
@@ -106,9 +66,14 @@ function ContextProvider({ children }) {
         setProductPageQuantity(1)
     }
 
+    
+
+    
+    
+
         
     return (
-        <Context.Provider value={{handleSeeProductClick, currentProduct, productPageQuantity, featureLineOne, featureLineTwo, handleYouMayLikeClick, handleProductPageDecrement, handleProductPageIncrement, handleAddToCartClick, handleCartIconClick, isCartOpen, cart}}>
+        <Context.Provider value={{handleSeeProductClick, currentProduct, productPageQuantity, featureLineOne, featureLineTwo, handleYouMayLikeClick, handleProductPageDecrement, handleProductPageIncrement, handleAddToCartClick, handleCartIconClick, isCartOpen, cart, setCart}}>
             {children}
         </Context.Provider>
     )

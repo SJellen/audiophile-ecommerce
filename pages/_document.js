edc1943124c1055696fe4e64021007586afd50f9
SJellen <sjellen@me.com>
2021-07-23 11:17:09 -0400

    edit on document file

--- pages/_document.js
+++ pages/_document.js
@@ -12,11 +12,11 @@ class MyDocument extends Document {
   render() {
 
     return (
-      <Html>
+      <Html lang="en">
        <Head>
           <meta name="description" content="Audiophile" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com"  />
           <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet" />
        </Head>
         <body>

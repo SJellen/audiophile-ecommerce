550fa987ae8e6244bf47cba8ba034c5d176408f4
SJellen <sjellen@me.com>
2021-08-01 10:31:18 -0400

    OrderComplet right container set

--- components/OrderComplete.js
+++ components/OrderComplete.js
@@ -3,34 +3,37 @@ import styles from '../styles/OrderComplete.module.scss'
 import { Context} from '../context/Context'
 import Link from 'next/link'
 import Image from 'next/image'
+import useCheckoutLogic from '../logic/useCheckoutLogic'
 
 export default function OrderComplete() {
 
     const {isOrderComplete} = useContext(Context)
+    const {filteredCart, totalPrice} = useCheckoutLogic()
 
     return (
         <div style={{display: isOrderComplete ? '' : 'none'}} className={styles.container}>
             <div className={styles.topContainer}>
                 <Image src="/assets/shared/desktop/check-mark.svg" alt="check mark icon" width="64" height="64"  className={styles.svg}/>
-                <h1>THANK YOU FOR YOUR ORDER</h1>
+                <h1>THANK YOU<br></br> FOR YOUR ORDER</h1>
                 <p>You will receive an email confirmation shortly.</p>
-
             </div>
             <div className={styles.middleContainer}>
                 <div className={styles.leftContainer}>
                     
                 </div>
                 <div className={styles.rightContainer}>
+                    <div className={styles.totalContainer}>
+                        <p>GRAND TOTAL</p>
+                        <h3>$ {(totalPrice() + 50).toLocaleString("en-US")}</h3>
 
+                    </div>
                 </div>
-
             </div>
             <div className={styles.buttonBox}>
                 <Link href="/" >
                     <a onClick={() => handleHandleCartCheckoutClick()}>BACK TO HOME</a>
                 </Link>
             </div>
-           
         </div>
     )
 }
